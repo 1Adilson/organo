1722e0e79044965cb6f8f4662ad5bd8f46c36c8a
Adilson Junior <juniorkadosh12@gmail.com>
2023-06-08 15:23:00 -0300

    crate from componets and add in app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Banner from "./components/Banner/Banner";
+import Forms from "./components/Forms/Forms";
 
 function App() {
   return (
     <div className="App">
       <Banner />
+      <Forms />
     </div>
   );
 }
---
+++ src/components/Forms/Forms.js
@@ -0,0 +1,11 @@
+import "./Forms.css";
+const Forms = () => {
+  return (
+    <div className="text-form">
+      <label>Nome</label>
+      <input placeholder="Digite seu nome" />
+    </div>
+  );
+};
+
+export default Forms;

c1decb5263386b122bfb097e535592f5e1b44c16
Adilson Junior <juniorkadosh12@gmail.com>
2023-06-08 14:34:20 -0300

    create Banner component

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,9 +1,13 @@
+import "./Banner.css";
+
 function Banner() {
   return (
-    <img
-      src="/imagens/banner.png"
-      alt="O banner principal da página do Organo"
-    />
+    <header className="banner">
+      <img
+        src="/images/banner.png"
+        alt="O banner principal da página do Organo"
+      />
+    </header>
   );
 }
 

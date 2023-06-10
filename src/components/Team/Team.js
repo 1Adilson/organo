b6ec072be148ec3417f5dfb4710f23f2610382ea
unknown <juniorkadosh12@gmail.com>
2023-06-10 20:38:27 -0300

    fix show team container

--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -5,23 +5,25 @@ import User from "../User/User";
 
 const Team = (props) => {
   return (
-    <section
-      className="teamContainer"
-      style={{ backgroundColor: props.secundaryColor }}
-    >
-      <h3 style={{ borderColor: props.primaryColor }}>{props.name}</h3>
-      <div className="users">
-        {props.users.map((item) => {
-          return (
-            <User
-              userName={item.name}
-              userOficce={item.office}
-              userImg={item.image}
-            ></User>
-          );
-        })}
-      </div>
-    </section>
+    props.users.length > 0 && (
+      <section
+        className="teamContainer"
+        style={{ backgroundColor: props.secundaryColor }}
+      >
+        <h3 style={{ borderColor: props.primaryColor }}>{props.name}</h3>
+        <div className="users">
+          {props.users.map((item) => {
+            return (
+              <User
+                userName={item.name}
+                userOficce={item.office}
+                userImg={item.image}
+              ></User>
+            );
+          })}
+        </div>
+      </section>
+    )
   );
 };
 
